7949a226b0ae3630e3f87e3184548db9f6a58fff
市川陽菜 <ichikawaharuna@ichikawaharunanoMacBook-Air.local>
2024-09-02 20:47:00 +0900

    setup01

--- test/controllers/shops_controller_test.rb
+++ test/controllers/shops_controller_test.rb
@@ -2,7 +2,7 @@ require "test_helper"
 
 class ShopsControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
-    get shops_index_url
+    get shops_url
     assert_response :success
   end
 end
